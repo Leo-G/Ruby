440ed002a2451b6cc0930bd764d3c4049a689283
Leo G <leo@leog.in>
2013-11-11 20:59:27 +0530

    add rb script

---
+++ isbn13-barcode.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/env ruby
+##Author Leo G
+##This program will take in a 12 digit ISBN number and convert it to a 13 digit number
+# # Copyright 2013 Leonard Gonsalves leog.in
+#  #  #This program is free software: you can redistribute it and/or modify
+#   #   #   it under the terms of the GNU General Public License as published by
+#    #    #   the Free Software Foundation, either version 3 of the License, or
+#     #     #   (at your option) any later version.
+#      #      #
+#       #       #   This program is distributed in the hope that it will be useful,
+#        #        #   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#         #         #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#          #          #   GNU General Public License for more details.
+#           #           # 
+#            #            #   You should have received a copy of the GNU General Public License
+#             #             #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#             array=[9,7,8,0,1,4,3,0,0,7,2,3]
+#             array2=[]
+#             puts "#{array.length}"
+#             puts "#{array.inspect}"
+#             array.each_with_index do |item, index |
+#             if (index %2 == 0)
+#             puts "Multiplying by 1"
+#             puts "#{item*1}"
+#             array2.push(item*1)
+#             else
+#             puts"multiplying by 3"
+#             puts "#{item*3}"
+#             array2.push(item*3)
+#             #puts "#{array2.inspect}"
+#             end
+#             end
+#
+#             puts "#{array2.inspect}"
+#             #!/usr/bin/env ruby
+#             #Author Leo G
+#             #This program will take in a 12 digit ISBN number and convert it to a 13 digit number
+#              # Copyright 2013 Leonard Gonsalves leog.in
+#               #  #This program is free software: you can redistribute it and/or modify
+#                #   #   it under the terms of the GNU General Public License as published by
+#                 #    #   the Free Software Foundation, either version 3 of the License, or
+#                  #     #   (at your option) any later version.
+#                   #      #
+#                    #       #   This program is distributed in the hope that it will be useful,
+#                     #        #   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#                      #         #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#                       #          #   GNU General Public License for more details.
+#                        #           # 
+#                         #            #   You should have received a copy of the GNU General Public License
+#                          #             #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#                          array=[9,7,8,0,1,4,3,0,0,7,2,3]
+#                          array2=[]
+#                          puts "#{array.length}"
+#                          puts "#{array.inspect}"
+#                          array.each_with_index do |item, index |
+#                          if (index %2 == 0)
+#                          puts "Multiplying by 1"
+#                          puts "#{item*1}"
+#                          array2.push(item*1)
+#                          else
+#                          puts"multiplying by 3"
+#                          puts "#{item*3}"
+#                          array2.push(item*3)
+#                          #puts "#{array2.inspect}"
+#                          end
+#                          end
+#
+#                          puts "#{array2.inspect}"
+#                          
